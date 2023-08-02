6e3665f449e70328e6863c0602d4734fdeaf797f
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-02 21:30:27 +0530

    terraform

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -11,7 +11,7 @@ resource "aws_route53_record" "rabbitmq" {
   zone_id = "Z011249635V6ILR7WTU6W"
   name    = "Tirupathi"
   type    = "A"
-  ttl     = 30
+  ttl     = 30   # Time To Live
   records = [aws_instance.Tirupathi.private_ip]
   allow_overwrite = true              # if record already there destroy previous one and creates new record and updates ip address also
 }
