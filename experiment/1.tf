ba92e11146feac1f6b1055ab26cfaef65a9ffdbd
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-03 21:30:31 +0530

    terraform

--- experiment/1.tf
+++ experiment/1.tf
@@ -1,4 +1,4 @@
 
-output "max" {
-        value=" The max value is ${max(5, 12, 9)}"
+output "max_min" {
+        value=[" The max value is ${max(5, 12, 9)}" , " and he in value is ${min(5, 12, 9)}"]
 }
