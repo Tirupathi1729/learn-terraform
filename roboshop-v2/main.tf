05d83dc01b78823110aa484b2eb8636b80bf02d3
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-04 15:31:19 +0530

    terraform

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -7,3 +7,39 @@ variable "instance_type" {
 variable "security_group" {
   default = ["sg-0cd32fc66a74591e4"]
 }
+
+variable "zone_id" {
+  default = "Z011249635V6ILR7WTU6W"
+}
+variable "components" {
+  default = {
+    frontend={name="frontend"}
+    mongod={name="mongodb"}
+    catalogue={name="catalogue"}
+    redis={name="redis"}
+    user={name="user"}
+    cart={name="cart"}
+    mysql={name="mysql"}
+    shipping={name="shipping"}
+    rabbitmq={name="rabbitmq"}
+  }
+}
+resource "aws_instance" "instance" {
+  for_each = var.components
+  ami           = var.amid_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group
+
+  tags = {
+    Name = lookup(each.value,"name",null )
+  }
+}
+resource "aws_route53_record" "record" {
+  for_each = var.components
+  zone_id = var.zone_id
+  name    = "${lookup(each.value,"name",null)}"
+  type    = "A"
+  ttl     = 30
+  records = [lookup(lookup(aws_instance.instance,each.key,null),"private_ip",null)]
+  #allow_overwrite = true              # if record already there destroy previous one and creates new record and updates ip address also
+}
