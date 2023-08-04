bd00b79aaa946ed5f782fa542cbee68c50a7e0b1
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-04 12:09:54 +0530

    terraform

--- experiment/vars.tf
+++ experiment/vars.tf
@@ -7,12 +7,3 @@ resource "aws_instance" "instance" {
     Name = "Tirupathi"
   }
 }
-variable "amid_id" {
-  default = "ami-03265a0778a880afb"
-}
-variable "instance_type" {
-  default = "t2.micro"
-}
-variable "security_group" {
-  default = ["sg-0cd32fc66a74591e4"]
-}
