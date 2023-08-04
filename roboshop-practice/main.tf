59b7bc0e24c83bcc8007998a1f200c0ad2bbf842
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-04 20:23:45 +0530

    terraform

--- roboshop-practice/main.tf
+++ roboshop-practice/main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = var.security_group
 
   tags = {
-    Name = lookup("components",each.key,null )
+    Name = lookup(each.values,"names",null )
   }
 }
 resource "aws_route53_record" "record" {
