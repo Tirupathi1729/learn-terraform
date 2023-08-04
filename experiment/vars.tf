d3aad5f468250e82d77ad8a0c0b9b9fc038b52f7
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-04 11:59:34 +0530

    terraform

---
+++ experiment/vars.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "instance" {
+  ami : var.amid_id
+  instance_type = var.instance_type
+}
+variable "amid_id" {
+  default = "ami-03265a0778a880afb"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
