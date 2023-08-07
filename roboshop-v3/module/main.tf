a6e12172aa1c21385dd6866a5235288ada8a8678
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-07 14:17:06 +0530

    terraform

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -1,13 +1,14 @@
 resource "aws_instance" "instance" {
-  ami           = data.aws_ami.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.security_groups
+  ami                       = data.aws_ami.ami.id
+  instance_type             = var.instance_type
+  vpc_security_group_ids    = var.security_groups
 
   tags = {
     Name = var.name
   }
 }
 resource "aws_route53_record" "record" {
+
   zone_id = var.zone_id
   name    = "${var.name}-dev.tirupathib74.online"
   type    = "A"
