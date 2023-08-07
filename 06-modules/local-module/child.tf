5090e8f2b4ec3e47bc7f4fdcc3d38ff12ff8a2e0
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-07 20:06:47 +0530

    terraform

--- 06-modules/local-module/child.tf
+++ 06-modules/local-module/child.tf
@@ -6,10 +6,10 @@ output "ami" {
   value = data.aws_ami_ids.ami
 }
 
-data "aws_ami_ids" "ami1" {
+data "aws_ami_ids" "ami" {
   name_regex = "centos"
   owners = ["973714476881"]
 }
 output "ami1" {
-  value = data.aws_ami_ids.ami1
+  value = data.aws_ami_ids.ami
 }
