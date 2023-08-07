30d3b14aa4c7be9779ba42c9fe4e876e8210d6b1
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-07 20:04:54 +0530

    terraform

--- 06-modules/root.tf
+++ 06-modules/root.tf
@@ -2,5 +2,8 @@ module "test" {
   source = "./local-module"
 }
 output "all_local_module" {
-  value = module.test
+  value = module.test       #both two output blocks will print
+}
+output "all_local_module1" {
+  value = module.test.ami1  # it prints only ami1 block
 }
