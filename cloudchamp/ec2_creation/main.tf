c178f59e0c0f08786749765448c573437d957abc
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-21 10:09:56 +0530

    terraform

--- cloudchamp/ec2_creation/main.tf
+++ cloudchamp/ec2_creation/main.tf
@@ -7,3 +7,45 @@ resource "aws_instance" "instance" {
     Name = "1st_instance"
   }
 }
+
+resource "aws_security_group" "Tf_SG" {
+  name        = "security group using terraform"
+  description = "security group using terraform"
+  vpc_id      = "vpc-095dcad0c8ac8c419"
+
+  ingress {
+    description      = "https"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "http"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "TF_SG"
+  }
+}
